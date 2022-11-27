7588759d305b1a8ba1106ee047149d7559bdf6be
AcquahBaiden <jbaiden@live.com>
2022-11-26 21:05:49 -0500

    Added table.js and imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Table from './table'
 
 class App extends Component {
     render() {
       return (
-        <div className="App">
-          <h1>Hello, React!</h1>
-        </div>
+        <div className="container">
+        <Table />
+      </div>
       )
     }
   }
