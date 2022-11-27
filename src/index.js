5b0a0827af9076b066a0766ce925bf2d4a9a826b
AcquahBaiden <jbaiden@live.com>
2022-11-26 21:02:53 -0500

    Added App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
-  }
-}
 ReactDOM.render(<App />, document.getElementById('root'))
