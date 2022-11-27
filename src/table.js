64f21cb563f641306f31427b6cb738b0f92f8960
AcquahBaiden <jbaiden@live.com>
2022-11-26 21:08:49 -0500

    Broke down table class into simple components

--- src/table.js
+++ src/table.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react'
 
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+const TableBody = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    )
+  }
+  
 class Table extends Component {
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+        <table>
+        <TableHeader />
+        <TableBody />
       </table>
     )
   }
 }
+
 
 export default Table
