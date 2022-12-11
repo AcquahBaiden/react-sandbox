1b606a6ab89d1eefb0458b59fe3866cf0d6b16c5
Acquah Baiden <jbaiden@live.com>
2022-12-10 21:27:19 -0500

    Update index.js
    Updated main import

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import './index.css';
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('try'))
+ReactDOM.render(<App />, document.getElementById('error'))
